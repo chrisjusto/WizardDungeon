c490e1d32645fdc8edb561d8f125ef0ec5806393
chrisjusto <chris.justo96@gmail.com>
2026-03-07 16:53:49 -0800

    update sprites

--- wizard-dungeon-v-2/actors/base_character.gd
+++ wizard-dungeon-v-2/actors/base_character.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collision: CollisionShape2D = $CollisionShape2D
-var speed = 60
+var speed = 60.0
 const acceleration: int = 10
 const friction: int = 30
 
@@ -21,74 +21,12 @@ var current_state = State.IDLE
 var current_direction = Facing.DOWN
 
 func _physics_process(delta: float) -> void:
-		 #Get raw input
 	var input = Vector2(
 		Input.get_action_strength("ui_right") - Input.get_action_raw_strength("ui_left"),
 		Input.get_action_strength("ui_down") - Input.get_action_raw_strength("ui_up")
 	).normalized()
-	if(input.x == 0 && input.y == 0):
-		current_state = State.IDLE
-	elif(input.y < 0):
-		current_state = State.WALKING
-		current_direction = Facing.UP
-	elif(input.y > 0):
-		current_state = State.WALKING
-		current_direction = Facing.DOWN
-	elif(input.x > 0):
-		current_state = State.WALKING
-		current_direction = Facing.LEFT	
-	elif(input.x < 0):
-		current_state = State.WALKING
-		current_direction = Facing.RIGHT	
-	#elif(input.x > 0 && input.y > 0):
-		#current_state = State.WALKING
-		#current_direction = Facing.UP
-	#elif(input.x > 0 && input.y < 0):
-		#current_state = State.WALKING
-		#current_direction = Facing.LEFT
-	#elif(input.x < 0 && input.y < 0):
-		#current_state = State.WALKING
-		#current_direction = Facing.RIGHT	
-	#elif(input.x < 0 && input.y > 0):
-		#current_state = State.WALKING
-		#current_direction = Facing.DOWN
-			
+	
 	velocity = input * speed
+	print(velocity)
 	move_and_slide()
 	global_position = global_position.round()
-	print(State)
-	print(Facing)
-	update_animation()
-
-
-
-func update_animation():
-	match current_state:
-		State.IDLE:
-			match current_direction:
-				Facing.UP:
-					sprite.animation = ("Idle_BackRight")
-					sprite.play()
-				Facing.DOWN:
-					sprite.animation = ("Idle_FrontLeft")
-					sprite.play()
-				Facing.LEFT:
-					sprite.animation =("Idle_FrontRight")
-					sprite.play()
-				Facing.RIGHT:
-					sprite.animation =("Idle_BackLeft")
-					sprite.play()
-		State.WALKING:
-			match current_direction:
-				Facing.UP:
-					sprite.animation =("Walk_BackRight")
-					sprite.play()
-				Facing.DOWN:
-					sprite.animation =("Walk_FrontLeft")
-					sprite.play()
-				Facing.LEFT:
-					sprite.animation =("Walk_FrontRight")
-					sprite.play()
-				Facing.RIGHT:
-					sprite.animation =("Walk_BackLeft")
-					sprite.play()
